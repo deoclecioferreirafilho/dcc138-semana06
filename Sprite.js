004dc223cdf22342e1dfebc6dae1db9b763c3ec7
Deoclecio Filho <deoclecio@ice.ufjf.com>
2019-09-19 20:10:29 -0300

    cruiado

---
+++ Sprite.js
@@ -0,0 +1,67 @@
+function Sprite(params = {}) {
+    var exemplo = {
+        x: 10,
+        y: 100,
+        h: 20,
+        w: 10,
+        vx: 0,
+        vy: 0,
+        ax: 0,
+        ay: 0,
+        a: 0, 
+        vm: 0,
+        props: {},
+        cooldown: 0,
+        va: 0,
+        color: "blue",
+        imune: 0,
+        atirando: 0,
+        scene: undefined,
+        comportar: undefined
+    }
+    Object.assign(this, exemplo, params);
+}
+
+Sprite.prototype = new Sprite({});
+Sprite.prototype.constructor = Sprite;
+
+Sprite.prototype.desenhar = function (ctx) {
+    ctx.save();
+    //ctx.fillRect(-this.w/2, -this.h/2, this.w, this.h);
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.a);
+    //ctx.strokeRect(-this.w/2, -this.h/2, this.w, this.h);
+    ctx.fillStyle = this.color;
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 1;
+
+    ctx.beginPath();
+    ctx.moveTo(-this.w / 2, -this.h / 2);
+    ctx.lineTo(-this.w / 2, +this.h / 2);
+    ctx.lineTo(+this.w / 2, 0)
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+    ctx.restore();
+};
+
+Sprite.prototype.mover = function (dt) {
+    this.a = this.a + this.va * dt;
+
+    this.vx = this.vm * Math.cos(this.a);
+    this.vy = this.vm * Math.sin(this.a);
+
+    this.x = this.x + this.vx * dt;
+    this.y = this.y + this.vy * dt;
+
+    this.cooldown = this.cooldown - dt;
+}
+Sprite.prototype.colidiuCom = function (alvo) {
+    if (alvo.x + alvo.w / 2 < this.x - this.w / 2) return false;
+    if (alvo.x - alvo.w / 2 > this.x + this.w / 2) return false;
+
+    if (alvo.y + alvo.h / 2 < this.y - this.h / 2) return false;
+    if (alvo.y - alvo.h / 2 > this.y + this.h / 2) return false;
+
+    return true;
+}
---
+++ assetsManager.js
@@ -0,0 +1,16 @@
+function AssetsManager(){
+    this.aCarregar = 0;
+    this.carregadas = 0;
+    this.assets = {};
+}
+AssetsManager.prototype.loadImage = function(key, url){
+    console.log (`Carregando imagem  ${url}...`)
+    this.aCarregar++;
+    var imagem = new Image();
+    console.log(`Imagem ${key}: ${url} carregada`);
+    imagem.addEventListener('load',function(){
+        this.carregadas++;
+    });
+    imagem.src =url;
+    this.assets[key] = imagem;
+}
