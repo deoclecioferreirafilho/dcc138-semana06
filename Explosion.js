01f87cf26ba9d708d6aa059bbf5b3dec31b498a5
Deoclecio Filho <deoclecio@ice.ufjf.com>
2019-09-26 22:07:38 -0300

    atualizado

--- Explosion.js
+++ Explosion.js
@@ -1,39 +1,34 @@
-function Explosion(){
-    var exemplo = {
+function Explosion(params = {}) {
+    exemplo = {
         x: 0,
         y: 0,
-        frame:0,
-        h:64,
+        frame: 0,
         w: 64,
+        h: 64,
         props: {
             tipo: "boom"
-        }
+        },
     }
     Object.assign(this, exemplo, params);
-
 }
-Explosion.prototype = new Explosion;
-Explosion.prototype.constructor = Explosion();
 
-
-Explosion.prototype.mover = function(dt){
-    this.frame += 26*dt;
-    if(Math,floor(this.frame) > 16){
-        this.frame = -1;
+Explosion.prototype.mover = function (dt) {
+    this.frame += 26 * dt;
+    if (Math.floor(this.frame) > 16) {
+        //this.frame = 0;
         this.morto = true;
     }
 }
 
-Explosion.prototype.desenhar = function(){
+Explosion.prototype.desenhar = function () {
     ctx.save();
     ctx.translate(this.x, this.y);
-    ctx.strokeRect(-this.w/2, -this.h/2, this.w, this.h);
     ctx.rotate(this.a + Math.PI / 2);
     var F = Math.floor(this.frame);
     ctx.drawImage(
         this.scene.assets.img("explosion"),
-        (F%4)*64,
-        Math.floor(F/4)*64,
+        (F % 4) * 64,
+        Math.floor(F / 4) * 64,
         64,
         64,
         -this.w / 2,
@@ -41,13 +36,13 @@ Explosion.prototype.desenhar = function(){
         this.w,
         this.h
     );
+    ctx.restore();
+}
+
+Explosion.prototype.comportar = function () {
 
 }
 
-Explosion.prototype.comportar = function(){
-
-}
-
-Explosion.prototype.colidiuCom = function(){
-
+Explosion.prototype.colidiuCom = function () {
+    return false;
 }
--- Scene.js
+++ Scene.js
@@ -3,12 +3,11 @@ function Scene(params) {
         sprites: [],
         toRemove: [],
         ctx: null,
-        w: 400,
-        h: 500,
+        w: 300,
+        h: 300,
         assets: null
     }
     Object.assign(this, exemplo, params);
-
 }
 
 Scene.prototype = new Scene();
@@ -17,53 +16,58 @@ Scene.prototype.constructor = Scene;
 Scene.prototype.adicionar = function (sprite) {
     this.sprites.push(sprite);
     sprite.scene = this;
-
-}
+};
 
 Scene.prototype.desenhar = function () {
-    for (var i = 0; i < this.sprites.length; i++)
+    for (var i = 0; i < this.sprites.length; i++) {
         this.sprites[i].desenhar(this.ctx);
-
-}
+    }
+};
 
 Scene.prototype.mover = function (dt) {
-    for (var i = 0; i < this.sprites.length; i++)
+    for (var i = 0; i < this.sprites.length; i++) {
         this.sprites[i].mover(dt);
+    }
+};
 
-}
-Scene.prototype.comportar = function (dt) {
+Scene.prototype.comportar = function () {
     for (var i = 0; i < this.sprites.length; i++) {
         if (this.sprites[i].comportar) {
             this.sprites[i].comportar();
         }
     }
+};
 
-}
 
 Scene.prototype.limpar = function () {
     this.ctx.clearRect(0, 0, this.w, this.h);
 }
 
-Scene.prototype.checaColisao = function (dt) {
+Scene.prototype.checaColisao = function () {
     for (var i = 0; i < this.sprites.length; i++) {
-        if(this.sprites[i].morto){
+        if (this.sprites[i].morto) {
             this.toRemove.push(this.sprites[i]);
         }
         for (var j = i + 1; j < this.sprites.length; j++) {
             if (this.sprites[i].colidiuCom(this.sprites[j])) {
-                if (this.sprites[i].props.tipo === "pc" && this.sprites[j].props.tipo === "npc") {
+                if (this.sprites[i].props.tipo === "pc"
+                    && this.sprites[j].props.tipo === "npc") {
                     this.toRemove.push(this.sprites[j]);
+                    this.adicionar(new Explosion({ x: this.sprites[j].x, y: this.sprites[j].y }));
+                    this.assets.play("explosion");
                 }
                 else
-                    if (this.sprites[i].props.tipo === "npc" && this.sprites[j].props.tipo === "tiro") {
+                    if (this.sprites[i].props.tipo === "npc"
+                        && this.sprites[j].props.tipo === "tiro") {
                         this.toRemove.push(this.sprites[i]);
                         this.toRemove.push(this.sprites[j]);
+                        this.adicionar(new Explosion({ x: this.sprites[i].x, y: this.sprites[i].y }));
+                        this.assets.play("explosion");
                     }
             }
         }
     }
-
-}
+};
 
 Scene.prototype.removeSprites = function () {
     for (var i = 0; i < this.toRemove.length; i++) {
@@ -73,7 +77,7 @@ Scene.prototype.removeSprites = function () {
         }
     }
     this.toRemove = [];
-}
+};
 
 Scene.prototype.passo = function (dt) {
     this.limpar();
@@ -82,8 +86,4 @@ Scene.prototype.passo = function (dt) {
     this.desenhar();
     this.checaColisao();
     this.removeSprites();
-}
-
-function newFunction() {
-    return "fillStyle = #dfe";
 }
